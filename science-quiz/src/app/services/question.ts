cfd5bd5f5aeb21c39a8534886d7a2a2097f7618f
diego <dcervantesapa@unsa.edu.pe>
2026-06-12 12:41:06 -0500

    agregando codigo de preguntas en question.ts

--- science-quiz/src/app/services/question.ts
+++ science-quiz/src/app/services/question.ts
@@ -1,4 +1,30 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-@Service()
-export class Question {}
+@Injectable({
+  providedIn: 'root'
+})
+export class Question {
+  preguntas = [
+    {
+      pregunta: '¿Qué lenguaje utiliza Angular?',
+      opciones: ['Python', 'TypeScript', 'PHP', 'Java'],
+      respuesta: 'TypeScript'
+    },
+    {
+      pregunta: '¿Qué comando carga el servidor Angular?',
+      opciones: ['npm start', 'ng serve', 'node app', 'angular run'],
+      respuesta: 'ng serve'
+    },
+    {
+      pregunta: '¿Qué directiva repite elementos?',
+      opciones: ['*ngIf', '*ngFor', '*ngSwitch', '*ngModel'],
+      respuesta: '*ngFor'
+    }
+  ];
+
+  constructor() {}
+
+  obtenerPreguntas() {
+    return this.preguntas;
+  }
+}
